f6fc51577abb016a42dabc285d770bc2a0b12f0e
anguie288 <61396665+anguie288@users.noreply.github.com>
2020-05-24 12:24:41 -0400

    Update period.js

--- period.js
+++ period.js
@@ -1,21 +1,21 @@
 // Military Add 2 hrs  5-17
-    /*from 1-2*/d01= new Date("May 18, 2020 17:30:00"); d11 = d01.getHours();d11m= d01.getMinutes();//d1=d01.getTime();d1=new Date(2020,05,18); 
+    /*from 1-2*/d01= new Date("May 20, 2020 12:30:00"); d11 = d01.getHours();d11m= d01.getMinutes();//d1=d01.getTime();d1=new Date(2020,05,18); 
     /*from 1-2*/var d02=new Date(d01);d02.setMinutes(d02.getMinutes() + 60); var d22=d02.getHours(); var d22m=d02.getMinutes();
     /*break 3*/var d03=new Date(d02); d03.setMinutes(d03.getMinutes()+15); var d33=d03.getHours(); var d33m=d03.getMinutes();
     
     /*from 3-4*/var d04=new Date(d03); d04.setMinutes(d04.getMinutes()+60); var d44=d04.getHours(); var d44m=d04.getMinutes();
     /*break 5*/var d05=new Date(d04);d05.setMinutes(d05.getMinutes() + 15); var d55=d05.getHours(); var d55m=d05.getMinutes();
     
-/* console.log( "period ("+(d11)+":"+d11m+") "+"to ("+(d22)+":"+d22m+") "+"sum ("+"1.00"+")");
-console.log( "break "+"("+(d22)+":"+(d22m)+")"+" to "+"("+(d33)+":"+(d33m)+")"+" sum ("+"0.25"+")\n");
+ console.log( "period ("+(d11)+":"+d11m+") "+"to ("+(d22)+":"+d22m+") "+"sum ("+"1.00"+")");
+console.log( "break  "+"("+(d22)+":"+(d22m)+")"+" to "+"("+(d33)+":"+(d33m)+")"+" sum ("+"0.25"+")\n");
 console.log( "period ("+(d33)+":"+d33m+") "+"to ("+(d44)+":"+d44m+") "+"sum ("+"1.00"+")");
-console.log( "break "+"("+(d44)+":"+(d44m)+")"+" to "+"("+(d55)+":"+(d55m)+")"+" sum ("+"0.25"+")\n");  */
+console.log( "break  "+"("+(d44)+":"+(d44m)+")"+" to "+"("+(d55)+":"+(d55m)+")"+" sum ("+"0.25"+")\n");  
 // afternoon
-console.log( "period ("+(d11-12)+":"+d11m+") "+"to ("+(d22-12)+":"+d22m+") "+"sum ("+"1.00"+")");
-console.log( "break "+"("+(d22-12)+":"+(d22m)+")"+" to "+"("+(d33-12)+":"+(d33m)+")"+" sum ("+"0.25"+")\n");
+/*console.log( "period ("+(d11-12)+":"+d11m+") "+"to ("+(d22-12)+":"+d22m+") "+"sum ("+"1.00"+")");
+console.log( "break  "+"("+(d22-12)+":"+(d22m)+")"+" to "+"("+(d33-12)+":"+(d33m)+")"+" sum ("+"0.25"+")\n");
 
 console.log( "period ("+(d33-12)+":"+d33m+") "+"to ("+(d44-12)+":"+d44m+") "+"sum ("+"1.00"+")");
-console.log( "break "+"("+(d44-12)+":"+(d44m)+")"+" to "+"("+(d55-12)+":"+(d55m)+")"+" sum ("+"0.25"+")\n");
+console.log( "break  "+"("+(d44-12)+":"+(d44m)+")"+" to "+"("+(d55-12)+":"+(d55m)+")"+" sum ("+"0.25"+")\n");*/
 
 /*  01 p.m. 1300  
     02 p.m. 1400
